4f9e5638471e57ad33ebafcaab9a7ae756e62a97
FStas <staszewskif98@gmail.com>
2025-03-19 16:12:56 +0000

    suma kwadratów losowych liczb z zakresu 0-100

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,10 @@ import java.util.Random;
 public class Main {
   public static void main(String[] args) {
     Random random = new Random();
+    double wynik = 0;
     
     for (int i = 0; i < 5; i++) {
       int liczba = random.nextInt(101);
-      double wynik = 0;
       wynik += Math.pow(liczba,2);
     }
     System.out.println(wynik);
